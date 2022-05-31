60c18e9159117f870f304c2a9370d57e253e84b8
chirag <chirag.supal@mobiuso.com>
2022-05-31 17:40:29 +0530

    Some Design and Chnages on UI

--- script.js
+++ script.js
@@ -2,25 +2,31 @@
 function findWinner(sysChose,userChoice){
     if(userChoice === "Stone" && sysChose === "scissors"){
         console.log("You Won. system chose "+sysChose);
-        document.getElementById('result').innerHTML = "You won. System Chose "+sysChose;
+        document.getElementById('result').innerHTML = "You won"+"</br> User Choice: "+userChoice+ "</br> System Choice: "+sysChose;
+        document.getElementById('result').style.color="green";
+        
     }
     else if(userChoice === "Scissors" && sysChose === "paper"){
         console.log("You Won. system chose "+sysChose);
-        document.getElementById('result').innerHTML = "You won. System Chose "+sysChose;
+        document.getElementById('result').innerHTML = "You won"+"</br> User Choice: "+userChoice+ "</br> System Choice: "+sysChose;
+        document.getElementById('result').style.color="green";
     }
     else if(userChoice === "Paper" && sysChose === "stone"){
         console.log("You Won. system chose "+sysChose);
-        document.getElementById('result').innerHTML = "You won. System Chose "+sysChose;
+        document.getElementById('result').innerHTML = "You won"+"</br> User Choice: "+userChoice+ "</br> System Choice: "+sysChose;
+        document.getElementById('result').style.color="green";
     }
     else if( userChoice === "Paper" && sysChose ==="paper" || 
             userChoice === "Stone" && sysChose ==="stone" || 
             userChoice === "Scissors" && sysChose ==="scissors" ) {
                 console.log("Tied. you both chose "+userChoice);
                 document.getElementById('result').innerHTML = "Tied. You both chose "+userChoice;
+                document.getElementById('result').style.color="green";
             }
     else{
         console.log("System Won. System chose "+sysChose);
-        document.getElementById('result').innerHTML = "System won. System Chose "+sysChose;
+        document.getElementById('result').innerHTML = "System won. System Chose "+sysChose+"</br> User Choice: "+userChoice+ "</br> System Choice: "+sysChose;
+        document.getElementById('result').style.color="red";
     }
 }
 
